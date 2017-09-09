6b102079d8f477845b6baeae3fc6fa4937060638
George Spake <gpspake@gmail.com>
2017-09-09 18:13:07 -0500

    wire up sign in scaffolding

--- src/Application.js
+++ src/Application.js
@@ -9,14 +9,28 @@ import './Application.css';
 class Application extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentUser:null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((currentUser) => {
+      console.log('AuthChange', currentUser);
+      this.setState({currentUser});
+    })
   }
 
   render() {
+    const { currentUser } = this.state;
+
     return (
       <div className="Application">
         <header className="Application--header">
           <h1>Lunch Rush</h1>
         </header>
+        {!currentUser && <SignIn/>}
+        {currentUser && <CurrentUser user={currentUser} />}
       </div>
     );
   }
